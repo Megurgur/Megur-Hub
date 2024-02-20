ccbef7032b324dd7f1a95df172e44f7a4c26f1e9
Megurgur <160540482+Megurgur@users.noreply.github.com>
2024-02-20 02:18:54 -0800

    Update Input.lua

--- Iris/Widgets/Input.lua
+++ Iris/Widgets/Input.lua
@@ -1427,6 +1427,7 @@ return function(Iris: Types.Internal, widgets: Types.WidgetUtility)
             InputField.MouseButton1Click:Connect(function()
                 task.wait()
                 local key = game:GetService("UserInputService").InputEnded:Wait()
+                key = key.KeyCode
                 thisWidget.state.key:set(key)
             end)
 
